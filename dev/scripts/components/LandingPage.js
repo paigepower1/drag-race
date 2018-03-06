03f9ddf35ea6a6c108078e3ce6e025faf13364b4
Lindsay Woods <lindsaywoods@live.ca>
2018-03-06 08:40:49 -0500

    styles on songcard section

--- dev/scripts/components/LandingPage.js
+++ dev/scripts/components/LandingPage.js
@@ -128,7 +128,7 @@ class LandingPage extends React.Component {
                         <li className="seasonTile" value="8" onClick={this.handleClick}>Season 8</li>
                         <li className="seasonTile" value="9" onClick={this.handleClick}>Season 9</li>
                     </ul>
-                <div className="songCardWrapper">
+                <div className="songCardWrapper" id="songCardWrapperID">
                     {this.state.songsFilteredBySeason.map((song, i) => {
                         return (
                             <SongInfo 
